9ab290a724508f6ea9898b833360566aeb19dccf
Sam Crudge <Samuelcrudge@gmail.com>
2020-03-23 15:52:43 +0000

    output

--- db.php
+++ db.php
@@ -3,6 +3,7 @@
 $db = new PDO ('mysql:host=DB;dbname=test', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
+$query = $db->query("SELECT `name`,`type`, `rating`,`desc` FROM `bread`;");
+$data = $query->fetchall();
 
-
-
+var_dump($data);
