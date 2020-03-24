c42d6e4b149b4c8bc7ec098df337b867fa6de203
Sam Crudge <Samuelcrudge@gmail.com>
2020-03-24 12:27:02 +0000

    added img's and style to them.

--- php/functions.php
+++ php/functions.php
@@ -3,12 +3,13 @@
 $db = new PDO ('mysql:host=DB;dbname=collectionapp', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$query = $db->query("SELECT `name`, `type`, `rating`, `desc` FROM `bread`;");
+$query = $db->query("SELECT `name`, `type`, `rating`, `desc`, `imgurl` FROM `bread`;");
 $breads = $query->fetchall();
 
 function loopbread($breads) {
     foreach ($breads as $bread){
         echo "<div class='listitem'>";
+            echo "<img src=".$bread['imgurl'].">";
             echo "<h1>".$bread['name']."</h1><br>";
             echo "Type: ".$bread['type']."  |";
             echo " Rating: ".$bread['rating']."<br>";
