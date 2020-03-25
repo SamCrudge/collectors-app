579b804879ae5c8d48f59e7852e71e32f54393c1
Sam Crudge <Samuelcrudge@gmail.com>
2020-03-25 12:13:45 +0000

    perentesies around die();

--- php/createItem.php
+++ php/createItem.php
@@ -6,8 +6,8 @@ $db = getDb();
 if (validateUsrInput($_POST)) {
     insertIntoDB($_POST, $db);
     header("location: ../index.php?success=1");
-    die;
+    die();
 } else {
     header("location: ../addBread.php?error=1");
-    die;
+    die();
 }
